eede59124ac4f999edbeb0c9842ca0ad444e1d95
Kallol Khatua <kallolkhatua2005@gmail.com>
2025-02-09 01:59:17 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get("/", async (req: Request, res: Response): Promise<void> => {
+    const response = {
+        statsCode: 200,
+        message: "connected",
+        success: true,
+        port: port,
+        env: process.env.DATA,
+        method: req.method,
+        path: req.path
+    };
+    res.status(200).send(response);
+})
+
+app.get("/hello-world", async (req: Request, res: Response): Promise<void> => {
+    const response = {
+        statsCode: 200,
+        message: "Hello world",
+        success: true,
+        port: port,
+        env: process.env.DATA,
+        method: req.method,
+        path: req.path
+    };
+    res.status(200).send(response);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
